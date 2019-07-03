746828bc61ed1b2e5089d1b1c745983c447c8060
Eric Miller <ericmailr@gmail.com>
2019-07-03 12:24:48 -0600

    update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181010161033) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
